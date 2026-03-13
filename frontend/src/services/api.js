4097d40ed76a85749d0b4b3b150e1a25178d64b7
Smit1911 <159136440+Smit1911@users.noreply.github.com>
2026-03-13 13:32:36 +0530

    Update Axios in api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const API_URL = import.meta.env.VITE_API_URL || "/api";
+axios.defaults.withCredentials = true;
 
 const api = axios.create({
   baseURL: API_URL,
